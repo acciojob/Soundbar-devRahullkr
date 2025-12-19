da6c18b420d86912e76a623a7d944f276cab5a53
Rahul Kr <rahulkumar8604745@gmail.com>
2025-12-19 21:19:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
 //your JS code here. If required.
-const sounds = {
-sound1: new Audio('sounds/sound1.mp3'),
-sound2: new Audio('sounds/sound2.mp3'),
-sound3: new Audio('sounds/sound3.mp3'),
-sound4: new Audio('sounds/sound4.mp3'),
-};
+const sounds = [
+'sound1.mp3',
+'sound2.mp3',
+'sound3.mp3',
+'sound4.mp3',
+'sound5.mp3',
+'sound6.mp3',
+];
 
 
-const buttons = document.querySelectorAll('.btn');
-const stopButton = document.querySelector('.stop');
+const buttonsContainer = document.getElementById('buttons');
+let currentAudio = null;
 
 
-function stopAllSounds() {
-for (let key in sounds) {
-sounds[key].pause();
-sounds[key].currentTime = 0;
-}
-}
+// Create a button for each sound
+sounds.forEach(sound => {
+const button = document.createElement('button');
+button.className = 'btn';
+button.innerText = sound.replace('.mp3', '');
 
 
-buttons.forEach((button) => {
 button.addEventListener('click', () => {
-const soundName = button.getAttribute('data-sound');
-stopAllSounds();
-sounds[soundName].play();
-});
+if (currentAudio) {
+currentAudio.pause();
+currentAudio.currentTime = 0;
+}
+
+
+currentAudio = new Audio(`sounds/${sound}`);
+currentAudio.play();
 });
 
 
-stopButton.addEventListener('click', stopAllSounds);
+buttonsContainer.appendChild(button);
+});
+const stopButton = document.createElement('button');
+stopButton.className = 'stop';
+stopButton.innerText = 'Stop';
+
+
+stopButton.addEventListener('click', () => {
+if (currentAudio) {
+currentAudio.pause();
+currentAudio.currentTime = 0;
+}
+});
+
+
+buttonsContainer.appendChild(stopButton);
